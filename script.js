54774e6af15ab12a6c8275c90bbe6897295ddb55
Sanjay Kushwaha <sanjaykushwaha80676@gmail.com>
2026-08-20 16:01:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name: string, age: number)
+	this.name = name ;
+	this.age  = age ;
+}
 
-class Student extends Person {}
+get changeName(){return this.name}
 
-class Teacher extends Person {}
+set changeAge(){return this.age}
+
+class Student extends Person {
+	constructor(name: string, age: number)
+
+	study(){
+		return(
+			Student.name + "is studying"
+		)
+	}
+}
+
+class Teacher extends Person {
+	constructor(name: string, age : number)
+
+	teach(){
+		return{
+			Teacher.name + "is teaching"
+		}
+	}
+}
+
+const Student1 = new Student("Sanjay", 28)
+const Teacher1 = new Teacher("Priyanshi", 26)
+console.log(Student1);
+console.log(Teacher1);
 
 // Do not change the code below this line
 window.Person = Person;
